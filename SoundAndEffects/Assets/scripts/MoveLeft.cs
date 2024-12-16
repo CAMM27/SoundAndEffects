a115aff4184e0d7a350f7051c6dcd896cdb87a18
CAMM27 <oneillc@roverkids.org>
2024-12-16 13:28:46 -0500

    when game over everything stops and destroys objects that exit bounds

--- SoundAndEffects/Assets/scripts/MoveLeft.cs
+++ SoundAndEffects/Assets/scripts/MoveLeft.cs
@@ -4,11 +4,12 @@ public class MoveLeft : MonoBehaviour
 {
     private float speed = 30;
     private PlayerController playerControllerScript;
+    private float leftBound = -15;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playerControllerScript = GameObject.Find("player").GetComponent<PlayerController>();
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -17,6 +18,11 @@ public class MoveLeft : MonoBehaviour
         if (playerControllerScript.gameOver == false)
         {
             transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
         }
        
     }
